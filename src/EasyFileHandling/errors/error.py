f83886a24bd5f2fad32516873833eb6fe1c70036
Jon Covington <jon.covington@gmail.com>
2021-08-30 13:45:00 -0600

    Added get_longest_line method to FileHandler class Added test for get_longest_line method

--- src/EasyFileHandling/errors/error.py
+++ src/EasyFileHandling/errors/error.py
@@ -2,13 +2,18 @@
 
 
 class FolderNotFound(Exception):
-    def __init__(self, message = "Folder or path not found"):
+    def __init__(self, message="Folder or path not found"):
         self.message = message
         super().__init__(self.message)
 
 
-
 class NotStringOrBytes(Exception):
-    def __init__(self, message = "Content must be string or bytes type object"):
+    def __init__(self, message="Content must be string or bytes type object"):
+        self.message = message
+        super().__init__(self.message)
+
+
+class ZeroLineFile(Exception):
+    def __init__(self, message="File has zero lines"):
         self.message = message
         super().__init__(self.message)
--- src/EasyFileHandling/tests/test_filehandler.py
+++ src/EasyFileHandling/tests/test_filehandler.py
@@ -1,6 +1,9 @@
 import unittest
 import os
+import sys
+import io
 from EasyFileHandling.main import FileHandler
+
 
 class FileHandlerTests(unittest.TestCase):
     test_filename = 'test.txt'
@@ -9,20 +12,31 @@ class FileHandlerTests(unittest.TestCase):
         try:
             with open(self.test_filename, 'w') as f:
                 f.write('This is a test file.\nThis is the second line.')
-        except PermissionError as err:
+        except PermissionError:
             self.fail('Unable to write test file.')
         self.fh = FileHandler(self.test_filename)
 
     def tearDown(self):
         os.remove(self.test_filename)
-    
+
     def test_does_file_exists_method(self):
         file_exists = self.fh.does_file_exist()
         self.assertTrue = file_exists
-    
+
     def test_get_line_count(self):
         line_count = self.fh.get_line_count()
         self.assertEqual(line_count, 2)
+
+    def test_get_longest_line(self):
+        capturedOutput = io.StringIO()
+        sys.stdout = capturedOutput
+        self.fh.get_longest_line()
+        sys.stdout = sys.__stdout__
+        self.assertEqual(
+            capturedOutput.getvalue().rstrip('\n'),
+            'Longest line is 24 at line index of [1]'
+            )
+
 
 if __name__ == '__main__':
     unittest.main()
